c0523da1765de7b2b9925f8a1b2d4c21203cbdfa
Rubenaso10 <rubenhq10.net@gmail.com>
2024-04-14 21:58:54 -0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './models/product.model';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-app';
+  http = inject(HttpClient)
+  products : Product [] = []
+
+  changeTitle(){
+    this.title = "title changed"
+  }
+
+  ngOnInit(){
+    this.http.get<Product[]>("https://api.escuelajs.co/api/v1/products").subscribe((data)=>{
+        this.products = data
+    })
+  }
+}
